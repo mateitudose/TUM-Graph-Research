e490f318b7a3a686d23289df9b0bae7161623e04
Matei Tudose <matei@mateitudose.dev>
2024-12-09 18:11:19 +0100

    Fix some error

--- tree_draw_jacob.py
+++ tree_draw_jacob.py
@@ -1,6 +1,7 @@
-import networkx as nx 
-import pylab as plt 
+import networkx as nx
+import pylab as plt
 from math import atan, gcd
+
 
 def generate_pythagorean_triplets(first_n):
     triplets = []
@@ -17,13 +18,14 @@ def generate_pythagorean_triplets(first_n):
                     return triplets
         m += 1
 
-def dfs(G:nx.Graph,root):
+
+def dfs(G: nx.Graph, root):
     stack = [root]
     visited = {root}
 
     for index in range(G.number_of_nodes()):
         u = stack.pop()
-        G.nodes[u]['order'] = index 
+        G.nodes[u]['order'] = index
 
         neighbors = [v for v in G.neighbors(u) if v not in visited]
         for v in neighbors:
@@ -31,53 +33,56 @@ def dfs(G:nx.Graph,root):
             stack.append(v)
             G.nodes[v]['parent'] = u
 
-def weighted_dfs(G:nx.Graph,root):
+
+def weighted_dfs(G: nx.Graph, root):
     pass
 
-def assign_pos(G:nx.Graph,root):
+
+def assign_pos(G: nx.Graph, root):
     n = G.number_of_nodes()
 
-    triplets = generate_pythagorean_triplets(n-1)
-    triplets.sort(key=lambda x: atan(x[1] / x[0]),reverse=True)
+    triplets = generate_pythagorean_triplets(n - 1)
+    triplets.sort(key=lambda x: atan(x[1] / x[0]), reverse=True)
 
-    process_order = sorted((v for v in G.nodes()),key=lambda v: G.nodes[v]['order'])
+    process_order = sorted((v for v in G.nodes()), key=lambda v: G.nodes[v]['order'])
 
     pos = dict()
     pos[root] = {'x': 0, 'y': 0}
 
-    for (a,b,c), v in zip(triplets,process_order[1:]):
+    for (a, b, c), v in zip(triplets, process_order[1:]):
         parent = G.nodes[v]['parent']
         pos[v] = {
-            'x': pos[parent] + a,
-            'y': pos[parent] + b
+            'x': pos[parent]['x'] + a,
+            'y': pos[parent]['y'] + b
         }
-    
+
     return pos
-        
-def tree_draw(G:nx.Graph,root,output=None):
 
-    dfs(G,root)
-    pos = assign_pos(G,root)
 
-    fig,ax = plt.subplots()
+def tree_draw(G: nx.Graph, root, output=None):
+    dfs(G, root)
+    pos = assign_pos(G, root)
 
-    for u,v in G.edges():
+    fig, ax = plt.subplots()
+
+    for u, v in G.edges():
         ax.plot(
             [pos[u]['x'], pos[v]['x']],
             [pos[u]['y'], pos[v]['y']],
             color='black'
         )
-    
-    if output is None: plt.show()
+
+    if output is None:
+        plt.show()
     else:
         fig.set_dpi(100)
         fig.savefig(output)
-    
+
     plt.close(fig)
 
+
 if __name__ == "__main__":
+    G = nx.random_tree(15)
+    root = 1
 
-    G = nx.random_tree(150)
-    root = 13
-
-    tree_draw(G,root)
+    tree_draw(G, root)
